d0d883edea72adc72374b637170f828b7450f4a2
Maxim Bilan <maximb.mail@gmail.com>
2015-04-19 18:46:28 +0300

    Added templates

---
+++ MBFileDownloader/Sources/MBFileDownloader.h
@@ -0,0 +1,13 @@
+//
+//  MBFileDownloader.h
+//  MBFileDownloader
+//
+//  Created by Maxim Bilan on 4/19/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBFileDownloader : NSObject
+
+@end
---
+++ MBFileDownloader/Sources/MBFileDownloader.m
@@ -0,0 +1,13 @@
+//
+//  MBFileDownloader.m
+//  MBFileDownloader
+//
+//  Created by Maxim Bilan on 4/19/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+#import "MBFileDownloader.h"
+
+@implementation MBFileDownloader
+
+@end
