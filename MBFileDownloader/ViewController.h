07d2ff9cb1d8acd4300214cb688f4a1d1b2f4388
Maxim Bilan <maximb.mail@gmail.com>
2015-04-19 18:44:22 +0300

    Added project template

---
+++ MBFileDownloader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MBFileDownloader
+//
+//  Created by Maxim Bilan on 4/19/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MBFileDownloader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MBFileDownloader
+//
+//  Created by Maxim Bilan on 4/19/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
