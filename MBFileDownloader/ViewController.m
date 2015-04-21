efa6c1a47ce062c7733655f4b216c80a25803c6e
Maxim Bilan <maximb.mail@gmail.com>
2015-04-21 20:32:18 +0300

    Added interface for testing

--- MBFileDownloader/ViewController.m
+++ MBFileDownloader/ViewController.m
@@ -10,18 +10,28 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *linkTextfile;
+@property (weak, nonatomic) IBOutlet UIButton *downloadButton;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
 	[super didReceiveMemoryWarning];
 	// Dispose of any resources that can be recreated.
+}
+
+- (IBAction)downloadAction:(UIButton *)sender
+{
+	
 }
 
 @end
